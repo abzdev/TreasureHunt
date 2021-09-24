a5c0f85aee8dba23d50dd120919192b16aa0bbf9
abzdev <76543409+abzdev@users.noreply.github.com>
2021-09-23 18:56:11 -0700

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,13 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        if(enteredCode.toUpperCase() === actualCode) return true;
+        
+    }
+
+}
